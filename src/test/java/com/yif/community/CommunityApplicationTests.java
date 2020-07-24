4b188b7a5daec9cb2591c04123a2b3b7ac8ba908
靳一飞 <1764061474@qq.com>
2020-07-24 20:05:54 +0800

    init project

---
+++ src/test/java/com/yif/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yif.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
