317c423683d676c97f40f2c9dadcc3ca8ce9986a
靳一飞 <1764061474@qq.com>
2020-07-28 19:51:32 +0800

    H2 change into mysql

---
+++ src/main/java/com/yif/community/mapper/QuestionMapper.java
@@ -0,0 +1,12 @@
+package com.yif.community.mapper;
+
+import com.yif.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag)values(#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create (Question quesion);
+}
